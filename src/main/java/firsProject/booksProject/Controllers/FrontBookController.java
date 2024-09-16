f9c84305687a9699d63b8e045c50e8cd60f945d4
aws <awskhalil999@gmail.com>
2024-09-17 02:39:57 +0300

    adding index

---
+++ src/main/java/firsProject/booksProject/Controllers/FrontBookController.java
@@ -0,0 +1,174 @@
+package firsProject.booksProject.Controllers;
+
+import firsProject.booksProject.Dtos.BookDto;
+import firsProject.booksProject.Exceptions.AuthorNotAddedException;
+import firsProject.booksProject.Exceptions.BookNotFoundException;
+import firsProject.booksProject.Exceptions.DateNotTrueException;
+import firsProject.booksProject.Exceptions.NumOfPublishException;
+import firsProject.booksProject.Services.BookService;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+
+import java.time.LocalDate;
+import java.time.ZoneOffset;
+import java.util.*;
+
+@Controller
+@EnableWebMvc
+@RequestMapping("/")
+public class FrontBookController {
+
+    private BookService bookService;
+    Set<String> authorstmp =new HashSet<>();
+    Set<String> authorstmp2 =new HashSet<>();
+
+    public FrontBookController(BookService bookService) {
+        this.bookService = bookService;
+    }
+
+    @GetMapping("/")
+    public String index()
+    {
+        return "index";
+    }
+    @GetMapping("/front/api/books/get")
+    public String getBooks(Model model) {
+        model.addAttribute("book", bookService.getAllBooks());
+        return "/allBooks";
+    }
+    @GetMapping("/front/api/books/add")
+    public String addBook(Model model) {
+        BookDto bookDto = new BookDto();
+        String author=new String();
+        model.addAttribute("book", bookDto);
+        model.addAttribute("author", author);
+        model.addAttribute("authorstmp", authorstmp);
+        return "addBook";
+    }
+    @GetMapping("/front/api/books/update/{id}")
+    public String updateForm(@PathVariable(value = "id") long id, Model model) {
+        BookDto bookDto = bookService.getBookById(id);
+        String author=new String();
+        model.addAttribute("authorstmp", authorstmp2);
+        model.addAttribute("author", author);
+        model.addAttribute("book", bookDto);
+        bookService.deleteBook(bookDto.getId());
+        return "modifyBook";
+    }
+    @PostMapping("/front/api/books/addAuthor")
+    public String addAuthor(@RequestParam String author){
+        if(!Objects.equals(author, ""))
+        {authorstmp.add(author);}
+        return "redirect:/front/api/books/add";
+    }
+    @PostMapping("/front/api/books/save")
+    public String saveBook(@ModelAttribute("book") BookDto bookDto) {
+        if(authorstmp.isEmpty()) {throw new AuthorNotAddedException("error there isnt any author here");}
+        bookDto.setAuthors(authorstmp);
+        if(bookDto.getDateOfPublish().compareTo(Date.from(LocalDate.now().atStartOfDay().toInstant(ZoneOffset.UTC))) == 1)
+        {throw new DateNotTrueException("date is not Acceptable");}
+        if(bookDto.getNumOfPublish()==0) throw new NumOfPublishException("numOfPublish is not acceptable");
+        bookService.addBook(bookDto);
+        authorstmp.clear();
+        return "redirect:/front/api/books/get";
+    }
+    @GetMapping("/front/api/books/deleteBook/{id}")
+    public String deleteThroughId(@PathVariable(value = "id") long id) {
+    bookService.deleteBook(id);
+    return "redirect:/front/api/books/get";
+}
+    @GetMapping("/front/api/books/searchId")
+    public String getId(Model model)
+    {
+     Long id = null;
+     model.addAttribute("id",id);
+     return "searchId";
+    }
+
+    @PostMapping("/front/api/books/findById")
+    public String findById(@ModelAttribute("id") long id){
+        return "redirect:getBookById/"+id;
+    }
+    @GetMapping("/front/api/books/getBookById/{id}")
+    public String getBookById(@PathVariable(value = "id") long id, Model model) {
+        if(id<=0) throw new BookNotFoundException("Id is not true");
+        model.addAttribute("book", bookService.getBookById(id));
+        return "SearchedById";
+    }
+
+    @GetMapping("/front/api/books/searchPublisher")
+    public String getPublisher(Model model)
+    {
+        String publisherName=null;
+        model.addAttribute("publisherName",publisherName);
+        return "searchPublisher";
+    }
+    @PostMapping("/front/api/books/findByPublisher")
+    public String findByPublisher(@ModelAttribute("publisherName") String publisherName ){
+    return "redirect:getBookByPublisher/"+publisherName;
+    }
+
+    @GetMapping("/front/api/books/getBookByPublisher/{publisherName}")
+    public String getBookByPublisher(@PathVariable(value = "publisherName") String publishername, Model model) {
+        model.addAttribute("book", bookService.getAllBooksByPublisher(publishername));
+        return "SearchedByPublisher";
+    }
+    @PostMapping("/front/api/books/getBookByAuthors/Final")
+    public String getBookByPublisher(Model model) {
+        model.addAttribute("book", bookService.getAllBooksByAuthors(authorstmp2.stream().toList()));
+        authorstmp2.clear();
+        return "SearchedByPublisher";
+    }
+    @PostMapping("/front/api/books/addAuthorForSearch")
+    public String addAuthorSearch(@RequestParam String author){
+        if(!Objects.equals(author, ""))
+        {authorstmp2.add(author);}
+        return "redirect:/front/api/books/searchAuthor";
+    }
+    @GetMapping("/front/api/books/searchAuthor")
+    public String searchAuthor(Model model) {
+        String author = null;
+        model.addAttribute("author", author);
+        return "searchAuthor";
+    }
+
+    @GetMapping("/front/api/books/searchTitle")
+    public String getTitle(Model model)
+    {
+       String title = null;
+        model.addAttribute("title",title);
+        return "searchTitle";
+    }
+
+    @PostMapping("/front/api/books/findByTitle")
+    public String findById(@ModelAttribute("title")  String title) {
+        return "redirect:getAllBooksByTitle/"+title;
+    }
+    @GetMapping("/front/api/books/getAllBooksByTitle/{title}")
+    public String getBookByTitle(@PathVariable(value = "title") String title, Model model) {
+        List<BookDto> books =bookService.getAllBooksByTitle(title);
+        model.addAttribute("book", books);
+        return "SearchedByTitle";
+    }
+
+    @GetMapping("/front/api/books/searchSummary")
+    public String getSummary(Model model)
+    {
+        String Summary = null;
+        model.addAttribute("summary",Summary);
+        return "searchSummary";
+    }
+
+    @PostMapping("/front/api/books/findBySummary")
+    public String findBySummary(@ModelAttribute("summary")  String summary) {
+        return "redirect:getBookBySummary/"+summary;
+    }
+    @GetMapping("/front/api/books/getBookBySummary/{summary}")
+    public String getBookBySummary(@PathVariable(value = "summary") String summary, Model model) {
+        model.addAttribute("book", bookService.getBookBySummary(summary));
+        return "SearchedBySummary";
+    }
+
+}
--- src/main/java/firsProject/booksProject/Entity/Book.java
+++ src/main/java/firsProject/booksProject/Entity/Book.java
@@ -10,10 +10,13 @@ import lombok.Setter;
 import java.util.Date;
 import java.util.Set;
 
+<<<<<<< Updated upstream
 @AllArgsConstructor
 @NoArgsConstructor
 @Setter
 @Getter
+=======
+>>>>>>> Stashed changes
 @Entity
 public class Book {
     @Id
@@ -34,4 +37,84 @@ public class Book {
     private Publisher publisher;
 
 
+<<<<<<< Updated upstream
+=======
+    public Book(long id, String title, String type, String summary, Date dateOfPublish, int numOfPublish, Set<String> authors, String publisher) {
+        this.id = id;
+        this.title = title;
+        this.type = type;
+        this.summary = summary;
+        this.dateOfPublish = dateOfPublish;
+        this.numOfPublish = numOfPublish;
+        this.authors = authors;
+        this.publisher = publisher;
+    }
+
+    public Book() {
+    }
+
+    public long getId() {
+        return id;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getType() {
+        return type;
+    }
+
+    public void setType(String type) {
+        this.type = type;
+    }
+
+    public String getSummary() {
+        return summary;
+    }
+
+    public void setSummary(String summary) {
+        this.summary = summary;
+    }
+
+    public Date getDateOfPublish() {
+        return dateOfPublish;
+    }
+
+    public void setDateOfPublish(Date dateOfPublish) {
+        this.dateOfPublish = dateOfPublish;
+    }
+
+    public int getNumOfPublish() {
+        return numOfPublish;
+    }
+
+    public void setNumOfPublish(int numOfPublish) {
+        this.numOfPublish = numOfPublish;
+    }
+
+    public Set<String> getAuthors() {
+        return authors;
+    }
+
+    public void setAuthors(Set<String> authors) {
+        this.authors = authors;
+    }
+
+    public String getPublisher() {
+        return publisher;
+    }
+
+    public void setPublisher(String publisher) {
+        this.publisher = publisher;
+    }
+>>>>>>> Stashed changes
 }
